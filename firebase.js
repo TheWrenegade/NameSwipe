c611bf02f977f05a86d680fb191f66cb30a8827f
Wren Bouwman <71395395+TheWrenegade@users.noreply.github.com>
2026-07-31 13:10:00 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLcUaI74zPK9TP9pV-pYY15CbtoiwCsXg",
+  authDomain: "baby-name-picker-fe571.firebaseapp.com",
+  projectId: "baby-name-picker-fe571",
+  storageBucket: "baby-name-picker-fe571.firebasestorage.app",
+  messagingSenderId: "714898625809",
+  appId: "1:714898625809:web:fa46a611966a0ef1c368b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
